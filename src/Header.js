30c7b5676b2d963f52e109e3bc38f0be739736bf
David Wieczorek <blackratio@gmail.com>
2019-01-07 10:13:58 +0100

    test style

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,13 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-import logo from './logo.svg';
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <menu>
-          <Link to="/">Home</Link>
-          <Link to="/players">PlayersList</Link>
-        </menu>
-      </div>
+      <menu>
+        <Link to="/">Home</Link>
+        <Link to="/players">PlayersList</Link>
+      </menu>
     );
   }
 }
